f1058351f8cb78eba0ff6f9d55e17f4fb5017809
Guddu <theguddu1730@gmail.com>
2022-06-09 19:03:15 +0530

    initial commit

---
+++ faulty calculator.py
@@ -0,0 +1,28 @@
+print('B.D.PUBLIC SCHOOL')
+print('ENTER YOUR 1st NUMBER')
+n1=float(input())
+print('ENTER THE OPERATOR')
+n2=input()
+print('ENTER YOUR 2nd NUMBER')
+n3=float(input())
+print("your calculation is ")
+
+if n1==34 and n2=="*" and n3==45:
+   print("4530")
+elif n1==457 and n2=="/" and  n3==56:
+   print("8.3")
+elif n1==672 and n2=="+" and n3==3442:
+    print("3824")
+
+
+if n1!=34 and n2 == "*":
+    print(n1*n3)
+elif n1!=672 and n2 == "+":
+    print(n1+n3)
+elif n1!=457 and n2 == "/":
+    print(n1/n3)
+elif n2 == "-":
+    print(n1-n3)
+else:
+    print("thank you for using me ")
+
